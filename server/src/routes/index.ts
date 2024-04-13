a3b3121a92343a15535c6a533ed1befe69a98c20
efforthye <efforthye@gmail.com>
2024-04-13 22:29:36 +0900

    feat: get html

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,22 @@
 import express, { Router } from 'express';
+import axios from 'axios';
 import { logger } from '../utils/logger';
 const url = '/api';
 
 const router: Router = express.Router();
+
+router.get('/html', async (req, res)=>{
+    try {
+        const {link} = req.query;
+        if(!link || typeof link !== 'string') throw Error;
+    
+        const html = (await axios.get(link)).data;
+        console.log({html});
+
+        res.send(html);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
 
 export { router, url };
